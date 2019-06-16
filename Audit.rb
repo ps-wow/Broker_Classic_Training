b2507ddf744442eb2009e6541dda26bd59c8499d
davidcraig <david@davidcraig.eu>
2019-06-16 16:12:19 +0100

    Update readme

---
+++ Audit.rb
@@ -0,0 +1,143 @@
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def brown
+    colorize(33)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
+
+
+
+classes = ['mage', 'hunter', 'druid', 'paladin', 'priest', 'rogue', 'shaman',
+  'warlock', 'warrior'
+]
+class_pets = ['warlockpet', 'hunterpet']
+
+class_audit = {}
+
+classes.each do |c|
+  class_audit[c] = {
+    spell_count: 0,
+    talent_count: 0,
+    tome_count: 0,
+    zero_id_spells: 0,
+    rankless_spells: 0
+  }
+end
+
+# Build arrays for final output
+spell_type = 'spell'
+
+classes.each do |c|
+  # Parse the file and check for spellId = 0
+  File.open("#{Dir.pwd}/#{c}.lua").each do |line|
+    # id = 0
+
+    if spell_type == 'spell'
+      if line.include?('id = 0')
+        class_audit[c][:zero_id_spells] = class_audit[c][:zero_id_spells] + 1
+      else
+        class_audit[c][:spell_count] = class_audit[c][:spell_count] + 1 if line.include?('id = ')
+      end
+
+      # Rankless Spells
+      class_audit[c][:rankless_spells] = class_audit[c][:rankless_spells] + 1 if line.include?('rank = 0')
+    end
+    
+
+    # Are we in talents yet?
+    spell_type = 'talent' if line.include?('TalentSpells')
+    # Are we in tomes yet
+    spell_type = 'tome' if line.include?('Tomes')
+
+    # Talents
+    class_audit[c][:talent_count] = class_audit[c][:talent_count] + 1 if line.include?('spellId') and spell_type == 'talent'
+
+    # Tomes
+    class_audit[c][:tome_count] = class_audit[c][:tome_count] + 1 if line.include?('spellId') and spell_type == 'tome'
+  end
+end
+
+##                        ##
+# Output the Audit Results #
+##                        ##
+
+def output_rated_value(value, zero_is_good = true)
+  if zero_is_good
+    if value == 0
+      return value.to_s.green
+    else
+      return value.to_s.red
+    end
+  else
+    if value == 0
+      return value.to_s.red
+    end
+
+    return value.to_s.green
+  end
+
+  value.to_s.green
+end
+
+# By Class
+classes.each do |c|
+  audit = class_audit[c]
+
+  case c
+  when 'mage'
+    puts c.upcase.light_blue
+  when 'hunter'
+    puts c.upcase.green
+  when 'rogue'
+    puts c.upcase.yellow
+  when 'shaman'
+    puts c.upcase.blue
+  when 'paladin'
+    puts c.upcase.pink
+  when 'warrior'
+    puts c.upcase.brown
+  else
+    puts c.upcase
+  end
+  
+  (c.length - 1).times { print "-" }
+  puts "-"
+
+  sep = " | "
+  output = ""
+  output << "Spells: #{output_rated_value(audit[:spell_count], false)}" << sep
+  output << "Zero ID: #{output_rated_value(audit[:zero_id_spells])}" << sep
+  output << "Talents: #{output_rated_value(audit[:talent_count], false)}" << sep
+  output << "Tomes: #{output_rated_value(audit[:tome_count], false)}"
+  print output
+  puts " "
+  puts " "
+end
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Project Status: Development
 
 This is an LDB (Lib Data Broker) plugin that provides a list of learnable spells for your class, now you don't need to visit your trainer to actually see if you have a new spell to pick up or not!
 
+Additionally the addon also has a database of the various Tomes for a given class and will list these also.
+
 # Feedback / Issues
 
 Currently spell listing might not be 100% accurate, if you find a spell thats missing or mis-categorised etc please raise an issue [here](https://github.com/ps-wow/Broker_Classic_Training/issues)
