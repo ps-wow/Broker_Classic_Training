637e4a2c73268d815472b7164de0653a85275dfe
davidcraig <david@davidcraig.eu>
2019-06-24 09:05:27 +0100

    Add rogue starting spells.

--- Libs/ClassSpells-1.0/rogue.lua
+++ Libs/ClassSpells-1.0/rogue.lua
@@ -1,21 +1,79 @@
 -- @see: https://classic.wowhead.com/rogue-abilities
 
+--- Automatic Spells
+-- Sinister Strike (Rank 1)
+-- Eviscerate (Rank 1)
+
 local RogueClassSpells = {
-  -- [6] = {
-  --   ['arcane_shot:1'] = {
-  --     name = 'Arcane Shot',
-  --     rank = 1,
-  --     id = 3044,
-  --     level = 6
-  --   }
-  -- },
-  -- [10] = {
-  --   ['dismiss_pet'] = {
-  --     name = 'Dismiss Pet',
-  --     level = 10,
-  --     id = 2641
-  --   }
-  -- }
+  [1] = {
+    ['stealth:1'] = {
+      name = 'Stealth',
+      rank = 1,
+      id = 0,
+      level = 1,
+      cost = 10
+    }
+  },
+  [2] = {},
+  [4] = {
+    ['backstab:1'] = {
+      name = 'Backstab',
+      rank = 1,
+      id = 0,
+      level = 4,
+      cost = 100
+    },
+    ['pick_pocket:1'] = {
+      name = 'Pick Pocket',
+      rank = 1,
+      id = 0,
+      level = 4,
+      cost = 100
+    }
+  },
+  [6] = {
+    ['gouge:1'] = {
+      name = 'Gouge',
+      rank = 1,
+      id = 0,
+      level = 6,
+      cost = 100
+    },
+    ['sinister_strike:2'] = {
+      name = 'Sinister Strike',
+      rank = 2,
+      id = 0,
+      level = 6,
+      cost = 100
+    }
+  },
+  [8] = {},
+  [10] = {},
+  [12] = {},
+  [14] = {},
+  [16] = {},
+  [18] = {},
+  [20] = {},
+  [22] = {},
+  [24] = {},
+  [26] = {},
+  [28] = {},
+  [30] = {},
+  [32] = {},
+  [34] = {},
+  [36] = {},
+  [38] = {},
+  [40] = {},
+  [42] = {},
+  [44] = {},
+  [46] = {},
+  [48] = {},
+  [50] = {},
+  [52] = {},
+  [54] = {},
+  [56] = {},
+  [58] = {},
+  [60] = {},
 }
 
 _G['Broker_Classic_Training_ROGUE'] = RogueClassSpells
