995d70a3133fae3bd51d0c5ab249b7820cadeb10
davidcraig <david@davidcraig.eu>
2019-06-24 15:05:32 +0100

    Add all arms spells.

--- Libs/ClassSpells-1.0/warrior.lua
+++ Libs/ClassSpells-1.0/warrior.lua
@@ -5,36 +5,352 @@ local WarriorClassSpells = {
     ['battle_shout:1'] = {
       name = 'Battle Shout',
       rank = 1,
-      id = 10,
-      level = 1
+      id = 6673,
+      level = 1,
+      cost = 10
     }
   },
   [4] = {
     ['charge:1'] = {
       name = 'Charge',
       rank = 1,
-      id = 100,
+      id = 0,
+      cost = 100,
       level = 4
     },
     ['rend:1'] = {
       name = 'Rend',
       rank = 1,
       id = 772,
-      level = 4
+      level = 4,
+      cost = 100
     }
   },
   [6] = {
     ['thunder_clap:1'] = {
       name = 'Thunder Clap',
       rank = 1,
-      id = 100,
-      level = 6
+      id = 0,
+      level = 6,
+      cost = 100
     },
     ['parry:1'] = {
       name = 'Parry',
       rank = 1,
-      id = 100,
-      level = 6
+      id = 0,
+      level = 6,
+      cost = 100
+    }
+  },
+  [8] = {
+    ['hamstring:1'] = {
+      name = 'Hamstring',
+      rank = 1,
+      id = 0,
+      level = 8,
+      cost = 200
+    },
+    ['heroic_strike:2'] = {
+      name = 'Heroic Strike',
+      rank = 2,
+      id = 0,
+      level = 8,
+      cost = 200
+    }
+  },
+  [10] = {
+    ['rend:2'] = {
+      name = 'Rend',
+      rank = 2,
+      id = 0,
+      level = 10,
+      cost = 600
+    }
+  },
+  [12] = {
+    ['overpower:1'] = {
+      name = 'Overpower',
+      rank = 1,
+      id = 0,
+      level = 12,
+      cost = 1000
+    }
+  },
+  [14] = {
+
+  },
+  [16] = {
+    ['heroic_strike:3'] = {
+      name = 'Heroic Strike',
+      rank = 3,
+      id = 0,
+      level = 16,
+      cost = 2000
+    },
+    ['mocking_blow:1'] = {
+      name = 'Mocking Blow',
+      rank = 1,
+      id = 0,
+      level = 16,
+      cost = 2000
+    }
+  },
+  [18] = {
+    ['thunder_clap:2'] = {
+      name = 'Thunder Clap',
+      rank = 2,
+      id = 0,
+      level = 18,
+      cost = 3000
+    }
+  },
+  [20] = {
+    ['Rend:3'] = {
+      name = 'Rend',
+      rank = 3,
+      id = 0,
+      level = 20,
+      cost = 4000
+    },
+    ['retaliation'] = {
+      name = 'Retaliation',
+      rank = 0,
+      id = 0,
+      level = 20,
+      cost = 4000
+    }
+  },
+  [22] = {
+
+  },
+  [24] = {
+    ['heroic_strike:4'] = {
+      name = 'Heroic Strike',
+      rank = 4,
+      id = 0,
+      level = 24,
+      cost = 8000
+    }
+  },
+  [26] = {
+    ['charge:2'] = {
+      name = 'Charge',
+      rank = 2,
+      id = 0,
+      level = 26,
+      cost = 10000
+    },
+    ['mocking_blow:2'] = {
+      name = 'Mocking Blow',
+      rank = 2,
+      id = 0,
+      level = 26,
+      cost = 10000
+    }
+  },
+  [28] = {
+    ['overpower:2'] = {
+      name = 'Overpower',
+      rank = 2,
+      id = 0,
+      level = 28,
+      cost = 11000
+    },
+    ['thunder_clap:3'] = {
+      name = 'Thunder Clap',
+      rank = 3,
+      id = 0,
+      level = 28,
+      cost = 11000
+    }
+  },
+  [30] = {
+    ['rend:4'] = {
+      name = 'Rend',
+      rank = 4,
+      id = 0,
+      level = 30,
+      cost = 12000
+    }
+  },
+  [32] = {
+    ['hamstring:2'] = {
+      name = 'Hamstring',
+      rank = 2,
+      id = 0,
+      level = 32,
+      cost = 14000
+    },
+    ['heroic_strike:5'] = {
+      name = 'Heroic Strike',
+      rank = 5,
+      id = 0,
+      level = 32,
+      cost = 14000
+    }
+  },
+  [34] = {
+
+  },
+  [36] = {
+    ['mocking_blow:3'] = {
+      name = 'Mocking Blow',
+      rank = 3,
+      id = 0,
+      level = 36,
+      cost = 18000
+    }
+  },
+  [38] = {
+    ['thunder_clap:4'] = {
+      name = 'Thunder Clap',
+      rank = 4,
+      id = 0,
+      level = 38,
+      cost = 20000
+    }
+  },
+  [40] = {
+    ['heroic_strike:6'] = {
+      name = 'Heroic Strike',
+      rank = 6,
+      id = 0,
+      level = 40,
+      cost = 22000
+    },
+    ['rend:5'] = {
+      name = 'Rend',
+      rank = 5,
+      id = 0,
+      level = 40,
+      cost = 22000
+    }
+  },
+  [42] = {
+
+  },
+  [44] = {
+    ['overpower:3'] = {
+      name = 'Overpower',
+      rank = 3,
+      id = 0,
+      level = 44,
+      cost = 34000
+    }
+  },
+  [46] = {
+    ['charge:3'] = {
+      name = 'Charge',
+      rank = 3,
+      id = 0,
+      level = 46,
+      cost = 36000
+    },
+    ['mocking_blow:4'] = {
+      name = 'Mocking Blow',
+      rank = 4,
+      id = 0,
+      level = 46,
+      cost = 36000
+    }
+  },
+  [48] = {
+    ['heroic_strike:7'] = {
+      name = 'Heroic Strike',
+      rank = 7,
+      id = 0,
+      level = 48,
+      cost = 40000
+    },
+    ['mortal_strike:2'] = {
+      name = 'Mortal Strike',
+      rank = 2,
+      id = 0,
+      level = 48,
+      cost = 200
+    },
+    ['thunder_clap:5'] = {
+      name = 'Thunder Clap',
+      rank = 5,
+      id = 0,
+      level = 48,
+      cost = 40000
+    }
+  },
+  [50] = {
+    ['rend:6'] = {
+      name = 'Rend',
+      rank = 6,
+      id = 0,
+      level = 50,
+      cost = 42000
+    }
+  },
+  [52] = {
+
+  },
+  [54] = {
+    ['hamstring:3'] = {
+      name = 'Hamstring',
+      rank = 3,
+      id = 0,
+      level = 54,
+      cost = 56000
+    },
+    ['mortal_strike:3'] = {
+      name = 'Mortal Strike',
+      rank = 3,
+      id = 0,
+      level = 54,
+      cost = 2800
+    }
+  },
+  [56] = {
+    ['heroic_strike:8'] = {
+      name = 'Heroic Strike',
+      rank = 8,
+      id = 0,
+      level = 56,
+      cost = 58000
+    },
+    ['mocking_blow:5'] = {
+      name = 'Mocking Blow',
+      rank = 5,
+      id = 0,
+      level = 56,
+      cost = 58000
+    }
+  },
+  [58] = {
+    ['thunder_clap:6'] = {
+      name = 'Thunder Clap',
+      rank = 6,
+      id = 0,
+      level = 58,
+      cost = 60000
+    }
+  },
+  [60] = {
+    ['mortal_strike:4'] = {
+      name = 'Mortal Strike',
+      rank = 4,
+      id = 0,
+      level = 60,
+      cost = 3100
+    },
+    ['overpower:4'] = {
+      name = 'Overpower',
+      rank = 4,
+      id = 0,
+      level = 60,
+      cost = 62000
+    },
+    ['rend:7'] = {
+      name = 'Rend',
+      rank = 7,
+      id = 0,
+      level = 60,
+      cost = 62000
     }
   }
 }
