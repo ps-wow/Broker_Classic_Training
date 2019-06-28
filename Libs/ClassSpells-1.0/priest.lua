c0ece584e733e3680577147badb7db7ee91e105e
davidcraig <david@davidcraig.eu>
2019-06-29 00:46:33 +0100

    Add all priest discipline spells

--- Libs/ClassSpells-1.0/priest.lua
+++ Libs/ClassSpells-1.0/priest.lua
@@ -7,7 +7,7 @@
 local PriestClassSpells = {
   -- Level 1: Total Cost 10c
   [1] = {
-    ['pw_fortitude:1'] = {
+    ['power_word_fortitude:1'] = {
       name = 'Power Word: Fortitude',
       rank = 1,
       id = 1243,
@@ -15,9 +15,11 @@ local PriestClassSpells = {
       cost = 10
     }
   },
-  -- Level 4: Total Cost 2s [Cumulative: 2s 10c]
+  [2] = {
+
+  },
   [4] = {
-    ['sw_pain:1'] = {
+    ['shadow_word_pain:1'] = {
       name = 'Shadow Word: Pain',
       rank = 1,
       id = 589,
@@ -33,7 +35,7 @@ local PriestClassSpells = {
     }
   },
   [6] = {
-    ['pw_shield:1'] = {
+    ['power_word_shield:1'] = {
       name = 'Power Word: Shield',
       rank = 1,
       id = 0,
@@ -81,7 +83,7 @@ local PriestClassSpells = {
       level = 12,
       cost = 800
     },
-    ['pw_fortitude:2'] = {
+    ['power_word_fortitude:2'] = {
       name = 'Power Word: Fortitude',
       rank = 2,
       id = 0,
@@ -89,6 +91,281 @@ local PriestClassSpells = {
       cost = 800
     }
   },
+  [14] = {
+
+  },
+  [16] = {
+
+  },
+  [18] = {
+    ['dispel_magic:1'] = {
+      name = 'Dispel Magic',
+      rank = 1,
+      id = 0,
+      level = 18,
+      cost = 2000
+    },
+    ['power_word_shield:3'] = {
+      name = 'Power Word: Shield',
+      rank = 3,
+      id = 0,
+      level = 18,
+      cost = 2000
+    }
+  },
+  [20] = {
+    ['inner_fire:2'] = {
+      name = 'Inner Fire',
+      rank = 2,
+      id = 0,
+      level = 20,
+      cost = 3000
+    },
+    ['shackle_undead:1'] = {
+      name = 'Shackle Undead',
+      rank = 1,
+      id = 0,
+      level = 20,
+      cost = 3000
+    }
+  },
+  [22] = {
+
+  },
+  [24] = {
+    ['mana_burn:1'] = {
+      name = 'Mana Burn',
+      rank = 1,
+      id = 0,
+      level = 24,
+      cost = 5000
+    },
+    ['power_word_fortitude:3'] = {
+      name = 'Power Word: Fortitude',
+      rank = 3,
+      id = 0,
+      level = 24,
+      cost = 5000
+    },
+    ['power_word_shield:4'] = {
+      name = 'Power Word: Shield',
+      rank = 4,
+      id = 0,
+      level = 24,
+      cost = 5000
+    }
+  },
+  [26] = {
+
+  },
+  [28] = {
+
+  },
+  [30] = {
+    ['inner_fire:3'] = {
+      name = 'Inner Fire',
+      rank = 3,
+      id = 0,
+      level = 30,
+      cost = 10000
+    },
+    ['power_word_shield:5'] = {
+      name = 'Power Word: Shield',
+      rank = 5,
+      id = 0,
+      level = 30,
+      cost = 10000
+    }
+  },
+  [32] = {
+    ['mana_burn:2'] = {
+      name = 'Mana Burn',
+      rank = 2,
+      id = 0,
+      level = 32,
+      cost = 11000
+    }
+  },
+  [34] = {
+    ['levitate'] = {
+      name = 'Levitate',
+      rank = 0,
+      id = 0,
+      level = 34,
+      cost = 12000
+    }
+  },
+  [36] = {
+    ['dispel_magic:2'] = {
+      name = 'Dispel Magic',
+      rank = 2,
+      id = 0,
+      level = 36,
+      cost = 14000
+    },
+    ['power_word_fortitude:4'] = {
+      name = 'Power Word: Fortitude',
+      rank = 4,
+      id = 0,
+      level = 36,
+      cost = 14000
+    },
+    ['power_word_shield:6'] = {
+      name = 'Power Word: Shield',
+      rank = 6,
+      id = 0,
+      level = 36,
+      cost = 14000
+    }
+  },
+  [38] = {
+
+  },
+  [40] = {
+    ['divine_spirit:2'] = {
+      name = 'Divine Spirit',
+      rank = 2,
+      id = 0,
+      level = 40,
+      cost = 900
+    },
+    ['inner_fire:4'] = {
+      name = 'Inner Fire',
+      rank = 4,
+      id = 0,
+      level = 40,
+      cost = 18000
+    },
+    ['mana_burn:3'] = {
+      name = 'Mana Burn',
+      rank = 3,
+      id = 0,
+      level = 40,
+      cost = 18000
+    },
+    ['shackle_undead:2'] = {
+      name = 'Shackle Undead',
+      rank = 2,
+      id = 0,
+      level = 40,
+      cost = 18000
+    }
+  },
+  [42] = {
+    ['power_word_shield:7'] = {
+      name = 'Power Word: Shield',
+      rank = 7,
+      id = 0,
+      level = 42,
+      cost = 22000
+    }
+  },
+  [44] = {
+
+  },
+  [46] = {
+
+  },
+  [48] = {
+    ['mana_burn:4'] = {
+      name = 'Mana Burn',
+      rank = 4,
+      id = 0,
+      level = 48,
+      cost = 28000
+    },
+    ['power_word_fortitude:5'] = {
+      name = 'Power Word: Fortitude',
+      rank = 5,
+      id = 0,
+      level = 48,
+      cost = 28000
+    },
+    ['power_word_shield:8'] = {
+      name = 'Power Word: Shield',
+      rank = 8,
+      id = 0,
+      level = 48,
+      cost = 28000
+    }
+  },
+  [50] = {
+    ['divine_spirit:3'] = {
+      name = 'Divine Spirit',
+      rank = 3,
+      id = 0,
+      level = 50,
+      cost = 1500
+    },
+    ['inner_fire:5'] = {
+      name = 'Inner Fire',
+      rank = 5,
+      id = 0,
+      level = 50,
+      cost = 30000
+    }
+  },
+  [52] = {
+
+  },
+  [54] = {
+    ['power_word_shield:9'] = {
+      name = 'Power Word: Shield',
+      rank = 9,
+      id = 0,
+      level = 54,
+      cost = 40000
+    }
+  },
+  [56] = {
+    ['mana_burn:5'] = {
+      name = 'Mana Burn',
+      rank = 5,
+      id = 0,
+      level = 56,
+      cost = 42000
+    }
+  },
+  [58] = {
+
+  },
+  [60] = {
+    ['divine_spirit:4'] = {
+      name = 'Divine Spirit',
+      rank = 4,
+      id = 0,
+      level = 60,
+      cost = 2300
+    },
+    ['inner_fire:4'] = {
+      name = 'Inner Fire',
+      rank = 4,
+      id = 0,
+      level = 60,
+      cost = 46000
+    },
+    ['power_word_fortitude:6'] = {
+      name = 'Power Word: Fortitude',
+      rank = 6,
+      id = 0,
+      level = 60,
+      cost = 46000
+    },
+    ['power_word_shield:10'] = {
+      name = 'Power Word: Shield',
+      rank = 10,
+      id = 0,
+      level = 60,
+      cost = 46000
+    },
+    ['prayer_of_spirit:1'] = {
+      name = 'Prayer of Spirit',
+      rank = 1,
+      id = 0,
+      level = 60,
+      cost = 2300
+    }
+  }
 }
 
 _G['Broker_Classic_Training_PRIEST'] = PriestClassSpells
